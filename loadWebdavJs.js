e1acc79334fcc34603364f1040b658a4c315294f
Sergey Ponomarev <stokito@gmail.com>
2023-04-09 11:12:39 +0300

    loadWebdavJs.js

--- loadWebdavJs.js
+++ loadWebdavJs.js
@@ -1,9 +1,11 @@
-console.log("TRY PROPFIND")
-let currentUrl = document.location.href
-fetch(currentUrl, {method: 'PROPFIND', headers: {Depth: 0}, credentials: "include"})
-	.then(response => response.text())
-	.then(propfindXml => checkPropfindResp(propfindXml))
-	.catch(error => console.log('Error:', error))
+function checkWebdavEnabled() {
+	console.log("Check for WebDAV: Try  PROPFIND")
+	let currentUrl = document.location.href
+	fetch(currentUrl, {method: 'PROPFIND', headers: {Depth: 0}, credentials: "include"})
+		.then(response => response.text())
+		.then(propfindXml => checkPropfindResp(propfindXml))
+		.catch(error => console.log('Error:', error))
+}
 
 function checkPropfindResp(propfindXml, tabId, davUrl) {
 	// Does it have <D:multistatus>?
@@ -18,3 +20,5 @@ function clearBody() {
 	console.log("Clear existing body if any")
 	document.body.innerHTML = ""
 }
+
+checkWebdavEnabled()
